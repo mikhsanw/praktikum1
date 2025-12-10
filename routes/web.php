5b1102ccc3ce8ecef53d3e84c69612ed44ee8164
Muhammad Ikhsan Wibowo <ikhsan.flip@gmail.com>
2025-12-11 01:29:05 +0700

    gasss

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::get('/', function () {
 Route::get('/file/{path}', function ($path) {
 
     // Path boleh mengandung subfolder
-    $path = 'public/'.$path;
+    $path = 'public/'.urldecode($path);
 
     if (! Storage::exists($path)) {
         abort(404);
