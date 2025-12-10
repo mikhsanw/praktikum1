9f5c3edfa2effae592371754b80a516a30e9971f
Muhammad Ikhsan Wibowo <ikhsan.flip@gmail.com>
2025-12-11 01:27:12 +0700

    d

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -51,7 +51,7 @@
                     <div class="flex items-center space-x-6 mt-2">
                         {{-- Current profile photo --}}
                         @if ($user->file)
-                            <img src="{{ url('/file/' . $user->file->path) }}" alt="Foto Profil"
+                            <img src="{{ url('/file/' . rawurlencode($user->file->path)) }}" alt="Foto Profil"
                                 class="w-20 h-20 rounded-full object-cover border">
                         @else
                             <div class="w-20 h-20 rounded-full bg-gray-200 flex items-center justify-center text-gray-500">
