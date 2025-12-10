8236e38f1b871b1c1bf4a40b73d24fe8e2d20175
Muhammad Ikhsan Wibowo <ikhsan.flip@gmail.com>
2025-12-11 01:06:55 +0700

    dd

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -35,7 +35,7 @@ class ProfileController extends Controller
             $extension = $file->getClientOriginalExtension();
             $filename = $user->id.'_'.time().'.'.$extension;
             $folder = 'profiles/'.$user->id;
-            $path = $file->storeAs($folder, $filename, 'public');
+            $path = $file->storeAs($folder, $filename);
 
             $user->file()->create([
                 'alias' => 'foto-profil',
